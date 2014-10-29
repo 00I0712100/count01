e88829588c84e9f43e027c97817c45feeba917a0
高橋知子 <Tomo@TakahashiTomoko-no-MacBook-Air.local>
2014-10-29 20:04:57 +0900

    Initial Commit

---
+++ Count/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Count
+//
+//  Created by 高橋知子 on 2014/10/29.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Count/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Count
+//
+//  Created by 高橋知子 on 2014/10/29.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
